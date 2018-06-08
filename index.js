3e6146353f4c20c11ae36771e6588289145c3dc5
Anurag <anurag.alw@gmail.com>
2018-06-08 22:28:05 +0530

    commit

--- index.js
+++ index.js
@@ -43,6 +43,21 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+var getDate = function () {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  today = dd + '-' + mm + '-' + yyyy;
+  return today;
+}
+
 
 
 app.get('/sendfile', function (req, res) {
@@ -60,7 +75,7 @@ app.get('/sendfile', function (req, res) {
       html: '<p>NSE Full data attached</p>',// plain text body
       attachments: [
         {
-          filename: "full.csv",
+          filename: "sec_bhavdata_full_" + getDate(),
           content: fileData
         }
       ]
